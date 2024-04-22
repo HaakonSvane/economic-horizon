35374fc3b6104d57537ee1e14476f4a452d6eccf
HaakonSvane <haakonsvane@gmail.com>
2024-04-22 23:04:03 +0000

    add balance projector

--- src/lib/finance/Projector.ts
+++ src/lib/finance/Projector.ts
@@ -20,4 +20,21 @@ export const projector = ({
   rate,
   ratePeriodTimeMillis,
 }: ProjectorInput): ProjectorReturn => {
-  
+  if (numSamples < 2) throw new Error("numSamples must be greater than 2");
+  const result: ProjectorReturn = [];
+  const timeStep = (endDate.getTime() - startDate.getTime()) / (numSamples - 1);
+
+  for (let i = 0; i < numSamples; i++) {
+    const ellapsedRateIterations = Math.floor(
+      (timeStep * i) / ratePeriodTimeMillis
+    );
+
+    const elapsedMillis = timeStep * i;
+    result.push({
+      time: new Date(startDate.getTime() + elapsedMillis),
+      balance: initialBalance * Math.pow(1 + rate, ellapsedRateIterations),
+    });
+  }
+
+  return result;
+};
